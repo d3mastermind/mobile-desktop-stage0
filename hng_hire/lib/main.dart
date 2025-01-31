8bc47495b02475a76d3b49cc4bc4b649dff0e465
d3mastermind <adewoleemmanuel36@gmail.com>
2025-01-30 22:05:15 -0500

    version one done

--- hng_hire/lib/main.dart
+++ hng_hire/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hng_hire/constants/tile_colors.dart';
 import 'package:hng_hire/widgets/talent_tile.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'HNG Hire',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
@@ -26,56 +28,185 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.white,
           centerTitle: true,
           title: Image.asset(
-              "mobile-desktop-stage0/hng_hire/lib/assets/images/logo.png"),
+            "lib/assets/images/logo.png",
+            width: 100,
+          ),
         ),
         backgroundColor: Colors.white,
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Text("Find and Hire Elite\n Freelance Talent!"),
-                  Text("We offer you the best Talents in any tech field."),
-                  Text("Discover over 20,000+ talents in the HNG network"),
-                  TalentTile(
-                    bgColor: TileColors.color1.color,
-                    title: "Flutter Developers",
-                    link: "http://hng.tech/hire/flutter-developers",
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _launchUrl(
+                        link:
+                            "https://github.com/d3mastermind/mobile-desktop-stage0");
+                  },
+                  child: SizedBox(
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 20,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Expanded(
+                              child:
+                                  Image.asset("lib/assets/images/github.png")),
+                          Icon(
+                            size: 50,
+                            Icons.navigate_next_rounded,
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  TalentTile(
-                    bgColor: TileColors.color2.color,
-                    title: "React-Native Developers",
-                    link: "http://hng.tech/hire/react-native-developers",
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _launchUrl(link: "https://delve.fun/");
+                  },
+                  child: SizedBox(
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 20,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Image.asset(
+                            "lib/assets/images/delve.png",
+                            color: Colors.black,
+                          )),
+                          Icon(
+                            size: 50,
+                            Icons.navigate_next_rounded,
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  TalentTile(
-                    bgColor: TileColors.color3.color,
-                    title: "kotlin Developers",
-                    link: "http://hng.tech/hire/kotlin-developers",
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _launchUrl(
+                      link: "https://telex.im/",
+                    );
+                  },
+                  child: SizedBox(
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Image.asset(
+                            "lib/assets/images/telex.png",
+                          )),
+                          Icon(
+                            size: 50,
+                            Icons.navigate_next_rounded,
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  TalentTile(
-                    bgColor: TileColors.color1.color,
-                    title: "Mobile Developers",
-                    link: "http://hng.tech/hire/mobile-ui-ux-developers",
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Find and Hire Elite\n Freelance Talent!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
                   ),
-                  TalentTile(
-                    bgColor: TileColors.color3.color,
-                    title: "Android Developers",
-                    link: "http://hng.tech/hire/android-developers",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "We offer you the best Talents in any tech field.",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.lightBlue),
+                ),
+                Text(
+                  "Discover over 20,000+ talents in the HNG network",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.lightBlue.withAlpha(100),
                   ),
-                  TalentTile(
-                    bgColor: TileColors.color2.color,
-                    title: "iOS Developers",
-                    link: "http://hng.tech/hire/ios-developers",
-                  )
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TalentTile(
+                  bgColor: TileColors.color1.color,
+                  title: "Flutter Developers",
+                  link: "http://hng.tech/hire/flutter-developers",
+                ),
+                TalentTile(
+                  bgColor: TileColors.color2.color,
+                  title: "React-Native Developers",
+                  link: "http://hng.tech/hire/react-native-developers",
+                ),
+                TalentTile(
+                  bgColor: TileColors.color3.color,
+                  title: "Kotlin Developers",
+                  link: "http://hng.tech/hire/kotlin-developers",
+                ),
+                TalentTile(
+                  bgColor: TileColors.color1.color,
+                  title: "Mobile Developers",
+                  link: "http://hng.tech/hire/mobile-ui-ux-developers",
+                ),
+                TalentTile(
+                  bgColor: TileColors.color3.color,
+                  title: "Android Developers",
+                  link: "http://hng.tech/hire/android-developers",
+                ),
+                TalentTile(
+                  bgColor: TileColors.color2.color,
+                  title: "iOS Developers",
+                  link: "http://hng.tech/hire/ios-developers",
+                )
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+Future<void> _launchUrl({required String link, context}) async {
+  final Uri url = Uri.parse(link);
+  try {
+    final launched = await launchUrl(url, mode: LaunchMode.inAppWebView);
+    if (!launched) {
+      throw Exception('Could not launch link');
+    }
+  } catch (e) {
+    // Handle the error, maybe show a snackbar
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Could not open link: ${e.toString()}')),
+      );
+    }
   }
 }
--- hng_hire/lib/widgets/talent_tile.dart
+++ hng_hire/lib/widgets/talent_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TalentTile extends StatefulWidget {
   const TalentTile({
@@ -15,17 +16,62 @@ class TalentTile extends StatefulWidget {
 }
 
 class _TalentTileState extends State<TalentTile> {
+  Future<void> _launchUrl() async {
+    final Uri url = Uri.parse(widget.link);
+    try {
+      final launched = await launchUrl(url, mode: LaunchMode.inAppWebView);
+      if (!launched) {
+        throw Exception('Could not launch ${widget.link}');
+      }
+    } catch (e) {
+      // Handle the error, maybe show a snackbar
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Could not open link: ${e.toString()}')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: _launchUrl,
       child: Container(
-        height: 200,
+        height: 120,
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(vertical: 8),
         decoration: BoxDecoration(
+          color: widget.bgColor,
           borderRadius: BorderRadius.circular(25),
         ),
-        color: widget.bgColor,
-        child: Text(widget.link),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    widget.title,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Icon(
+                    size: 50,
+                    Icons.navigate_next_rounded,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
